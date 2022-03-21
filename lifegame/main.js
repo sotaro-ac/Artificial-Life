cfb9ecdb23f7e460fef1b4ce5914eb9f6e325698
sotaro-ac <yaasoo0623.academic@gmail.com>
2022-03-21 23:57:38 +0900

    add lifegame/

---
+++ lifegame/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// HTML Elements' IDs
+const elmCanvasId = 'canvas';
+
+// const canvas = document.getElementById(elmCanvasId);
+// const ctx = canvas.getContext('2d');
+
+// const stdLen = Math.min(canvas.clientWidth, window.innerHeight - 200);
+// // canvas.style.width = stdLen + "px";
+// // canvas.style.height = stdLen + "px";
+
+// // メモリ上における実際のサイズを設定(ピクセル密度の分だけ倍増する)
+// let scale = 1; // CSS解像度と物理解像度の比
+// if (high_quarity) {
+//     scale = window.devicePixelRatio;
+// }
+// canvas.width = stdLen * scale;
+// canvas.height = stdLen * scale;
+
+// // CSS上のピクセル数を前提としているシステムに合わせる
+// ctx.scale(scale, scale);
+// let size = stdLen / N;
+
+// // 補足情報テキストの描写
+// ctx.fillText(`HELLO WORLD!`, size, 2 * size);
