093f60b9b252a4495f1e36304c01a93a6f457681
sotaro-ac <yaasoo0623.academic@gmail.com>
2022-03-23 22:46:21 +0900

    ready to use canvas
    - jQueryを利用した

--- lifegame/main.js
+++ lifegame/main.js
@@ -1,26 +1,31 @@
 "use strict";
 
-// HTML Elements' IDs
-const elmCanvasId = 'canvas';
+/**
+ * Dependency: jquery-3.6.0.min.js
+ */
 
-// const canvas = document.getElementById(elmCanvasId);
-// const ctx = canvas.getContext('2d');
+const N = 100;
 
-// const stdLen = Math.min(canvas.clientWidth, window.innerHeight - 200);
-// // canvas.style.width = stdLen + "px";
-// // canvas.style.height = stdLen + "px";
+$(document).ready(function () {
+    // HTML Elements' IDs
+    const canvas = $("#canvas")[0];
+    const ctx = canvas.getContext('2d');
 
-// // メモリ上における実際のサイズを設定(ピクセル密度の分だけ倍増する)
-// let scale = 1; // CSS解像度と物理解像度の比
-// if (high_quarity) {
-//     scale = window.devicePixelRatio;
-// }
-// canvas.width = stdLen * scale;
-// canvas.height = stdLen * scale;
+    const stdLen = Math.min(canvas.clientWidth, window.innerHeight - 200);
 
-// // CSS上のピクセル数を前提としているシステムに合わせる
-// ctx.scale(scale, scale);
-// let size = stdLen / N;
+    // メモリ上における実際のサイズを設定(ピクセル密度の分だけ倍増する)
+    let scale = 1; // CSS解像度と物理解像度の比
+    // scale = window.devicePixelRatio;
 
-// // 補足情報テキストの描写
-// ctx.fillText(`HELLO WORLD!`, size, 2 * size);
+    canvas.width = stdLen * scale;
+    canvas.height = stdLen * scale;
+
+    // CSS上のピクセル数を前提としているシステムに合わせる
+    ctx.scale(scale, scale);
+    let size = stdLen / N;
+
+    // 補足情報テキストの描写
+    ctx.font = `bold ${5 * size}px 'VT323', cursive`;
+    ctx.fillStyle = "#000";
+    ctx.fillText(`HELLO WORLD!`, (canvas.width - 20 * size) / 2, canvas.height / 2);
+});
