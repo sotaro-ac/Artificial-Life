3ff49ff810482209d0d65b8a097aa18388d441be
sotaro-ac <yaasoo0623.academic@gmail.com>
2022-03-23 23:57:44 +0900

    fill random -> canvas

--- lifegame/main.js
+++ lifegame/main.js
@@ -1,10 +1,27 @@
 "use strict";
 
 /**
- * Dependency: jquery-3.6.0.min.js
+ * Dependencies:
+ *  - https://code.jquery.com/jquery-3.6.0.slim.min.js
+ *  - https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.1.0/chroma.min.js
  */
 
+// 
 const N = 100;
+const cmap = chroma.scale(['whitesmoke', 'green']).domain([0, 1]);
+const cell = [];
+const getRandom = (range) => { return Math.floor(Math.random() * range); };
+
+for (let i = 0; i < N; i++) {
+    cell.push([]);
+    for (let j = 0; j < N; j++) {
+        cell[i].push(getRandom(2));
+    }
+}
+
+function update(canvas, y, x) {
+
+}
 
 $(document).ready(function () {
     // HTML Elements' IDs
@@ -23,6 +40,16 @@ $(document).ready(function () {
     // CSS上のピクセル数を前提としているシステムに合わせる
     ctx.scale(scale, scale);
     let size = stdLen / N;
+
+    // セルの描写
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; j++) {
+            let [r, g, b] = cmap(cell[i][j]).rgb();
+            ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+            // fillRect(開始x座標, 開始y座標, 描画幅, 描画高さ)
+            ctx.fillRect(j * size, i * size, size, size);
+        }
+    }
 
     // 補足情報テキストの描写
     ctx.font = `bold ${5 * size}px 'VT323', cursive`;
